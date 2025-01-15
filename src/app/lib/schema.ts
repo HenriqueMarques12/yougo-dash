86a3439d18095963ce91d6f5cd4938c47b681212
Fabricio Siqueira <blackfchanel@gmail.com>
2025-01-15 16:13:20 -0300

    FEAT: form campos parceiro, estado e cidade depenedo do tipo de privilegio

--- src/app/lib/schema.ts
+++ src/app/lib/schema.ts
@@ -18,11 +18,16 @@ export interface UserType {
     planoFinish: string | null,
     status: string | null,
     statusVenda: string,
-    parceiro: string | null,
+    parceiro: string,
     createdAt: string,
     updatedAt: string,
     pdvs: []
   } 
+
+  export interface CidadeType {
+    id: number,
+    nome: string,
+  }
 
 export const FormDataEditSchema = z.object({
     nome: z.string().nonempty("Nome é obrigatorio"),
@@ -33,6 +38,32 @@ export const FormDataEditSchema = z.object({
     telefone: z.string().nonempty("Telefone é obrigatorio").min(15, {message: "Deve conter 15 caracteres"}),
     plano: z.string().nonempty("Plano é obrigatorio"),
 })
+
+export const FormDataVendedorEditSchema = z.object({
+    nome: z.string().nonempty("Nome é obrigatorio"),
+    email: z.string().nonempty("Email é obrigatorio"),
+    password: z.string().nonempty("Senha é obrigatorio").min(6, {message: "Senha deve conter no minimo 6 caracteres"}),
+    cpf: z.string().nonempty("CPF é obrigatorio").min(14, {message: "Deve conter 14 caracteres"}),
+    dataNascimento: z.string().nonempty("Data Nascimento é obrigatorio").min(10, {message: "Deve conter 10 caracteres"}),
+    telefone: z.string().nonempty("Telefone é obrigatorio").min(15, {message: "Deve conter 15 caracteres"}),
+    estado: z.string(),
+    cidade: z.string(),
+    parceiro: z.string(),
+})
+
+
+export const FormDataAdminParceiroEditSchema = z.object({
+    nome: z.string().nonempty("Nome é obrigatorio"),
+    email: z.string().nonempty("Email é obrigatorio"),
+    password: z.string().nonempty("Senha é obrigatorio").min(6, {message: "Senha deve conter no minimo 6 caracteres"}),
+    cpf: z.string().nonempty("CPF é obrigatorio").min(14, {message: "Deve conter 14 caracteres"}),
+    dataNascimento: z.string().nonempty("Data Nascimento é obrigatorio").min(10, {message: "Deve conter 10 caracteres"}),
+    telefone: z.string().nonempty("Telefone é obrigatorio").min(15, {message: "Deve conter 15 caracteres"}),
+    estado: z.string(),
+    cidade: z.string(),
+})
+
+
 
 
 export const FormDataLoginSchema = z.object({
